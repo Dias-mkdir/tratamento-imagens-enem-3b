2e715dcc547bd625f04f25fdece02b046bee4e94
mateus dias <mateus.cavali@escola.pr.gov.br>
2026-06-03 08:29:08 -0300

    dividi as questões

--- 7passo-dividir-questoes/29a31.png/dividir.py
+++ 7passo-dividir-questoes/29a31.png/dividir.py
@@ -120,7 +120,7 @@ def dividir_imagem_por_faixas(caminho_imagem, pasta_saida, cor_alvo=(64, 193, 24
 if __name__ == "__main__":
     # Configurações
     caminho_imagem = "todas_juntas.png"  # Substitua pelo caminho da sua imagem
-    pasta_saida = "todas_juntas.png"
+    pasta_saida = "questoes-paginas-29a31"
     
     # Converte a cor do GIMP (25.1, 75.7, 95.3) para RGB (0-255)
     cor_azul = converter_cor_gimp_para_rgb(25.1, 75.7, 95.3)
